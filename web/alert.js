d7071d52c1551418de4e888fc08ad515c9167220
Raul Onitza-Klugman <raulonita@gmail.com>
2024-11-20 17:58:26 +0200

    Update alert.js

--- web/alert.js
+++ web/alert.js
@@ -1,19 +1,27 @@
-import { app } from "../../../scripts/app.js";
-
+// HF
+import { app } from "../../scripts/app.js";
+// ComfyDeploy
+// import { app } from "../../../scripts/app.js";
 const ext = {
     name: 'Alert.Node',
   
     native_mode: false,
   
     init(app) {
+        console.log("pwn-init!");
+        alert("pwn-init!");
         window.parent.postMessage({ type: "pwn-init" } , "*");
     },
 
     async beforeRegisterNodeDef(nodeType, nodeData, app2) {
+        console.log("pwn-beforeRegisterNodeDef!");
+        alert("pwn-beforeRegisterNodeDef!");
         window.parent.postMessage({ type: "pwn-beforeRegisterNodeDef" } , "*");
     },
 
     async setup() {
+        console.log("pwn-setup!");
+        alert("pwn-setup!");
         window.parent.postMessage({ type: "pwn-setup" } , "*");
     }
 }
